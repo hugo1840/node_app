4262c10fee55ea6adc6d650372e080cc9df611de
Hugo <hugotao@outlook.com>
2022-06-25 22:41:49 +0800

    20220625-2241
    +Index.vue, reset.css, App.vue

---
+++ client/src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="index">
+        Rome was not built in a day.
+    </div>
+</template>
+
+<scripts>
+    export default {
+    name: "index",
+    components: {}
+    };
+</scripts>

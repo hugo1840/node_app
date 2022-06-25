d31ee7171042bab30693b5622f9df40ca7098620
Hugo <hugotao@outlook.com>
2022-06-26 00:20:26 +0800

    20220626-0019 updates
    +Register.vue, 404.vue, router/index.js

---
+++ client/src/views/Register.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="register">
+        <section class="form_container">
+            <div class="manage_tip">
+                <span class="title">Kisu Management Platform</span>
+            </div>
+        </section>
+    </div>
+</template>
+
+<scripts>
+    export default {
+    name: "register",
+    components: {}
+    };
+</scripts>
+
+<style scoped>
+    .register {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        background: url(../assets/bg.jpg) no-repeat center center;
+        background-size: 100% 100%;
+    }
+    .form_container {
+        width: 370px;
+        height: 210px;
+        position: absolute;
+        top: 10%;
+        left: 34%;
+        padding: 25px;
+        border-radius: 5px;
+        text-align: center;
+    }
+
+    .form_container .manage_tip .title {
+        font-family: "Microsoft YaHei";
+        font-weight: bold;
+        font-size: 26px;
+        color: #fff;
+    }
+</style>

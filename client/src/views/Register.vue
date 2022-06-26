61ea50e276b5f687fb1013b9c2349f0f9c99b065
Hugo <hugotao@outlook.com>
2022-06-26 16:50:41 +0800

    20220626-1650 updates
    +element-ui in Register.vue

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -3,17 +3,51 @@
         <section class="form_container">
             <div class="manage_tip">
                 <span class="title">Kisu Management Platform</span>
+                <el-form :model="registerUser" :rules="rules" ref="registerForm" label-width="80px" class="registerForm">
+                    <el-form-item label="用户名" prop="name">
+                        <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="registerUser.email" placeholder="请输入邮箱地址"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input type="password" v-model="registerUser.password" placeholder="请输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="确认密码" prop="password2">
+                        <el-input type="password" v-model="registerUser.password2" placeholder="请再次输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="选择身份">
+                        <el-select v-model="registerUser.identity" placeholder="请选择身份">
+                            <el-option label="经理" value="manager"></el-option>
+                            <el-option label="员工" value="employee"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button typeof="primary" class="submit_btn" @click="submitForm('registerForm')">注册</el-button>
+                    </el-form-item>
+                </el-form>
             </div>
         </section>
     </div>
 </template>
 
-<scripts>
+<script>
     export default {
     name: "register",
-    components: {}
-    };
-</scripts>
+    components: {},
+    data(){
+      return {
+        registerUser: {
+          name: '',
+          email: '',
+          password: '',
+          password2: '',
+          identity: ''
+        }
+      }
+    }
+ };
+</script>
 
 <style scoped>
     .register {
@@ -39,5 +73,16 @@
         font-weight: bold;
         font-size: 26px;
         color: #fff;
+    }
+    .registerForm {
+        margin-top: 20px;
+        background-color: #fff;
+        padding: 20px 40px 20px 20px;
+        border-radius: 5px;
+        box-shadow: 0px 5px 10px #cccc;
+    }
+
+    .submit_btn {
+        width: 100%;
     }
 </style>
